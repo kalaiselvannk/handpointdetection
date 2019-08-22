f537139680cdb793185554f8cb7087c01f45e164
kalaiselvannk <kalaiselvannk@rediffmail.com>
2019-08-22 00:13:21 -0700

    dataset download scripts added

---
+++ download_dataset.sh
@@ -0,0 +1,4 @@
+wget http://domedb.perception.cs.cmu.edu/panopticDB/hands/hand_labels.zip
+wget http://domedb.perception.cs.cmu.edu/panopticDB/hands/hand_labels_synth.zip
+unzip -q hand_labels.zip
+unzip -q hand_labels_synth.zip
